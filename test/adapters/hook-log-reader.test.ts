7c929af3f6b334779a59cb552c33c72e3a026ad9
Harshit Tomar <147972668+harshittomar0@users.noreply.github.com>
2026-08-12 16:16:01 +0530

    Update hook-log-reader.test.ts

--- test/adapters/hook-log-reader.test.ts
+++ test/adapters/hook-log-reader.test.ts
@@ -1,4 +1,4 @@
-import { test, describe } from "node:test";
+import { test, describe } from "vitest";
 import assert from "node:assert/strict";
 import * as fs from "node:fs";
 import * as os from "node:os";
